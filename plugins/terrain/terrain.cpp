0791da0eb5aa8b3ee321b8d40dcfea6d633fef76
aodq <0@aodq.net>
2023-02-18 21:41:18 -0800

    add editor capability

--- plugins/terrain/terrain.cpp
+++ plugins/terrain/terrain.cpp
@@ -17,6 +17,8 @@ struct Context {
   PuleGfxShaderModule shaderModule;
   PuleGfxGpuBuffer bufferAttributesStatic;
   PuleGfxPipeline pipeline;
+
+  size_t terrainVertices;
 };
 
 Context ctx;
@@ -25,24 +27,65 @@ struct TerrainMeshAttribute {
   PuleF32v3 origin;
 };
 
-void initializeContext() {
+void initializeContext(
+  float const * const heights,
+  size_t const width, size_t const height,
+  bool isEditor=false
+) {
   PuleError err = puleError();
 
-  std::vector<TerrainMeshAttribute> attributes = {
-    { .origin = { PuleF32v3 { -3.0f, 0.0f, -3.0f, }, }, },
-    { .origin = { PuleF32v3 { +3.0f, 0.0f, -3.0f, }, }, },
-    { .origin = { PuleF32v3 { +3.0f, 0.0f, +3.0f, }, }, },
-    { .origin = { PuleF32v3 { +3.0f, 0.0f, +3.0f, }, }, },
-    { .origin = { PuleF32v3 { -3.0f, 0.0f, +3.0f, }, }, },
-    { .origin = { PuleF32v3 { -3.0f, 0.0f, -3.0f, }, }, },
-  };
+  float const mapdim = 100.0f;
+
+  std::vector<TerrainMeshAttribute> attributes;
+  { // parse heightfield to mesh
+    float originStartX = -mapdim/2.0f;
+    float originStartY = -mapdim/2.0f;
+    float originItX = mapdim/(float)width;
+    float originItY = mapdim/(float)height;
+
+    for (size_t itx = 0; itx < width-1; ++ itx)
+    for (size_t ity = 0; ity < height-1; ++ ity) {
+      auto const ul = PuleF32v3 {
+        originStartX + itx*originItX,
+        heights[ity*height + itx],
+        originStartY + ity*originItY,
+      };
+      auto const ur = PuleF32v3 {
+        originStartX + (itx+1)*originItX,
+        heights[ity*height + (itx+1)],
+        originStartY + ity*originItY,
+      };
+      auto const ll = PuleF32v3 {
+        originStartX + itx*originItX,
+        heights[(ity+1)*height + itx],
+        originStartY + (ity+1)*originItY,
+      };
+      auto const lr = PuleF32v3 {
+        originStartX + (itx+1)*originItX,
+        heights[(ity+1)*height + (itx+1)],
+        originStartY + (ity+1)*originItY,
+      };
+      puleLog("<%f, %f, %f> -> <%f, %f, %f>", ul.x, ul.y,ul.z, lr.x,lr.y,lr.z);
+      attributes.emplace_back(ul);
+      attributes.emplace_back(ur);
+      attributes.emplace_back(lr);
+      attributes.emplace_back(lr);
+      attributes.emplace_back(ll);
+      attributes.emplace_back(ul);
+    }
+  }
+  ctx.terrainVertices = attributes.size();
 
   ctx.bufferAttributesStatic = (
     pul.gfxGpuBufferCreate(
       attributes.data(),
       sizeof(TerrainMeshAttribute) * attributes.size(),
       PuleGfxGpuBufferUsage_bufferAttribute,
-      PuleGfxGpuBufferVisibilityFlag_deviceOnly
+      (
+        isEditor
+        ? PuleGfxGpuBufferVisibilityFlag_hostWritable
+        : PuleGfxGpuBufferVisibilityFlag_deviceOnly
+      )
     )
   );
 
@@ -105,9 +148,9 @@ void initializeContext() {
         .blendEnabled = false,
         .scissorTestEnabled = false,
         .viewportUl = PuleI32v2 { 0, 0, },
-        .viewportLr = PuleI32v2 { 800, 600, },
+        .viewportLr = PuleI32v2 { 400, 400, },
         .scissorUl = PuleI32v2 { 0, 0, },
-        .scissorLr = PuleI32v2 { 800, 600, },
+        .scissorLr = PuleI32v2 { 400, 400, },
       },
     };
 
@@ -127,40 +170,10 @@ void initializeContext() {
   /* } */
 }
 
-} // namespace
-
-extern "C" {
-
-PulePluginType pulcPluginType() {
-  return PulePluginType_component;
-}
-
-void pulcComponentLoad(PulePluginPayload const payload) {
-  ::pul = *reinterpret_cast<PuleEngineLayer *>(
-    pulePluginPayloadFetch(payload, puleCStr("pule-engine-layer"))
-  );
-
-  initializeContext();
-}
-
-void pulcComponentUpdate(PulePluginPayload const payload) {
-  auto const taskGraph = PuleTaskGraph {
-    .id = pul.pluginPayloadFetchU64(
-      payload,
-      pul.cStr("pule-render-task-graph")
-    ),
-  };
-  PuleTaskGraphNode const renderGeometryNode = (
-    pul.taskGraphNodeFetch(taskGraph, pul.cStr("render-geometry"))
-  );
-  auto const recorder = PuleGfxCommandListRecorder {
-    pul.taskGraphNodeAttributeFetchU64(
-      renderGeometryNode,
-      pul.cStr("command-list-primary-recorder")
-    )
-  };
-
-
+void terrainRender(
+  PuleGfxFramebuffer const framebuffer,
+  PuleGfxCommandListRecorder const recorder
+) {
   pul.gfxCommandListAppendAction(
     recorder,
     PuleGfxCommand {
@@ -170,12 +183,21 @@ void pulcComponentUpdate(PulePluginPayload const payload) {
       },
     }
   );
+  pul.gfxCommandListAppendAction(
+    recorder,
+    PuleGfxCommand {
+      .bindFramebuffer = {
+        .action = PuleGfxAction_bindFramebuffer,
+        .framebuffer = framebuffer,
+      },
+    }
+  );
   static float time = 0.0f;
   time += 1.0f/60.0f;
   { // push constant
     PuleF32m44 const view = (
       puleViewLookAt(
-        PuleF32v3{sinf(time)*3.0f, 1.0f + 0.5f*cosf(time*0.5f), cosf(time)*3.0f},
+        PuleF32v3{sinf(time)*3.0f, 5.0f + 0.5f*cosf(time*0.5f), cosf(time)*3.0f},
         puleF32v3(0.0),
         PuleF32v3{0.0f, 1.0f, 0.0f}
       )
@@ -214,10 +236,204 @@ void pulcComponentUpdate(PulePluginPayload const payload) {
         .action = PuleGfxAction_dispatchRender,
         .drawPrimitive = PuleGfxDrawPrimitive_triangle,
         .vertexOffset = 0,
-        .numVertices = 6,
+        .numVertices = ctx.terrainVertices,
       },
     }
   );
+}
+
+} // namespace
+
+extern "C" {
+
+PulePluginType pulcPluginType() {
+  return PulePluginType_component;
+}
+
+void pulcComponentLoad(PulePluginPayload const payload) {
+  ::pul = *reinterpret_cast<PuleEngineLayer *>(
+    pulePluginPayloadFetch(payload, puleCStr("pule-engine-layer"))
+  );
+
+  // TODO load
+  std::vector<float> heights = { 1.0f, 2.0f, 1.0f, 1.0f };
+  initializeContext(heights.data(), 2, 2, false);
+}
+
+void pulcComponentUpdate(PulePluginPayload const payload) {
+  auto const taskGraph = PuleTaskGraph {
+    .id = pul.pluginPayloadFetchU64(
+      payload,
+      pul.cStr("pule-render-task-graph")
+    ),
+  };
+  PuleTaskGraphNode const renderGeometryNode = (
+    pul.taskGraphNodeFetch(taskGraph, pul.cStr("render-geometry"))
+  );
+  auto const recorder = PuleGfxCommandListRecorder {
+    pul.taskGraphNodeAttributeFetchU64(
+      renderGeometryNode,
+      pul.cStr("command-list-primary-recorder")
+    )
+  };
+
+  /* ::terrainRender(PuleGfxFramebuffer{0}, recorder); */
+}
+
+} // extern C
+
+// -- editor -------------------------------------------------------------------
+#include <pulchritude-imgui/imgui.h>
+
+namespace {
+
+PuleGfxFramebuffer guiFramebuffer;
+PuleGfxGpuImage guiImageColor;
+PuleGfxGpuImage guiImageDepth;
+PuleGfxCommandList guiCommandList;
+PuleGfxCommandListRecorder guiCommandListRecorder;
+std::vector<float> guiHeightmap;
+void * guiMappedAttributes;
+
+void guiInitialize() {
+  static bool initialized = false;
+  if (initialized) { return; }
+  initialized = true;
+
+  PuleDsValue const dsTerrain = puleDsCreateObject(puleAllocateDefault());
+  { // store default terrain
+    std::vector<float> defaultTerrainValues;
+    defaultTerrainValues.resize(100*100);
+    for (size_t itx = 0; itx < 100; ++ itx)
+    for (size_t ity = 0; ity < 100; ++ ity) {
+      defaultTerrainValues.emplace_back(1.0f + (itx%20)*5.0f + (ity%50)*6.5f);
+    }
+    guiHeightmap = defaultTerrainValues;
+    (void)dsTerrain;
+    /* puleDsObjectMemberAssign( */
+    /*   dsTerrain, */
+    /*   puleCStr("heightmap-buffer"), */
+    /*   puleDsCreateBuffer( */
+    /*     puelAllocateDefault(), */
+    /*     PuleArrayView { */
+    /*       .data = defaultTerrainValues.data(), */
+    /*       .elementStride = sizeof(float), */
+    /*       .elementCount = defaultTerrainValues.size() */
+    /*     } */
+    /*   ) */
+    /* ); */
+    /* puleDsObjectMemberAssign( */
+    /*   dsTerrain, puleCStr("heightmap-width"), puleDsCreateI64(100) */
+    /* ); */
+    /* puleDsObjectMemberAssign( */
+    /*   dsTerrain, puleCStr("heightmap-height"), puleDsCreateI64(100) */
+    /* ); */
+  }
+
+  // load terrain into context
+  initializeContext(guiHeightmap.data(), 100, 100, true);
+
+  // gui mapped pointers
+  guiMappedAttributes = (
+    pul.gfxGpuBufferMap({
+      .buffer = ctx.bufferAttributesStatic,
+      .access = PuleGfxGpuBufferMapAccess_hostWritable,
+      .byteOffset = 0,
+      .byteLength = sizeof(TerrainMeshAttribute) * 100*100,
+    })
+  );
+
+  // gui command list
+
+  guiCommandList = (
+    puleGfxCommandListCreate(puleAllocateDefault(), puleCStr("terrain-gui"))
+  );
+  guiCommandListRecorder = (
+    puleGfxCommandListRecorder(guiCommandList)
+  );
+
+  { // gui image / framebuffer
+    PuleGfxSampler sampler = (
+      puleGfxSamplerCreate({
+        .minify = PuleGfxImageMagnification_nearest,
+        .magnify = PuleGfxImageMagnification_nearest,
+        .wrapU = PuleGfxImageWrap_clampToEdge,
+        .wrapV = PuleGfxImageWrap_clampToEdge,
+      })
+    );
+    guiImageColor = (
+      puleGfxGpuImageCreate({
+        .width = 400,
+        .height = 400,
+        .target = PuleGfxImageTarget_i2D,
+        .byteFormat = PuleGfxImageByteFormat_rgba8U,
+        .sampler = sampler,
+        .optionalInitialData = nullptr,
+      })
+    );
+    guiImageDepth = (
+      puleGfxGpuImageCreate({
+        .width = 400,
+        .height = 400,
+        .target = PuleGfxImageTarget_i2D,
+        .byteFormat = PuleGfxImageByteFormat_depth16,
+        .sampler = sampler,
+        .optionalInitialData = nullptr,
+      })
+    );
+    PuleGfxFramebufferCreateInfo fbci = puleGfxFramebufferCreateInfo();
+    fbci.attachmentType = PuleGfxFramebufferType_imageStorage;
+    fbci.attachment.images[PuleGfxFramebufferAttachment_color0] = {
+      .image = guiImageColor, .mipmapLevel = 0,
+    };
+    fbci.attachment.images[PuleGfxFramebufferAttachment_depth] = {
+      .image = guiImageDepth, .mipmapLevel = 0,
+    };
+    PuleError err = puleError();
+    puleGfxFramebufferCreate(fbci, &err);
+    if (puleErrorConsume(&err)) { return; }
+  }
+}
+
+} // namespace
+
+extern "C" {
+void puldGuiEditor(
+  [[maybe_unused]] PuleAllocator const allocator,
+  [[maybe_unused]] PulePlatform const platform,
+  PuleEngineLayer const pulLayer
+) {
+  ::pul = pulLayer;
+  guiInitialize();
+  static bool open = true;
+  pul.imguiWindowBegin("terrain", &open);
+  if (!open) {
+    return;
+  }
+
+  pul.gfxCommandListRecorderReset(guiCommandListRecorder);
+  ::terrainRender(guiFramebuffer, guiCommandListRecorder);
+  pul.gfxCommandListRecorderFinish(guiCommandListRecorder);
+  PuleError err = pul.error();
+  pul.gfxCommandListSubmit(
+    PuleGfxCommandListSubmitInfo {
+      .commandList = guiCommandList,
+      .fenceTargetStart = nullptr,
+      .fenceTargetFinish = nullptr,
+    },
+    &err
+  );
+  if (pul.errorConsume(&err)) { return; }
+
+  pul.imguiImage(
+    guiImageColor, PuleF32v2{400, 400}, pul.f32v2(0), pul.f32v2(1),
+    pul.f32v4(1)
+  );
+  if (pul.imguiLastItemHovered()) {
+    pul.imguiText("hovered");
+  }
+
+  pul.imguiWindowEnd();
 }
 
 } // extern C
